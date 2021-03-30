45b5ce82f668e5c6ea1436019828e439ddf5f893
LukeHan <LukeHan1128@gmail.com>
2021-03-30 19:27:30 +0900

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,47 +8,31 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main extends JFrame{
     private static ArrayList<Point> plist = new ArrayList<Point>();
     private static ArrayList<String> gitlog = new ArrayList<String>();
 
-    private int tBold = 5;
-    private int cCircle = 12;
-    private int lineWidth = 20;
-    private int lineHeight = 20;
-    private int winWidth = 1200;
-    private int winHeight = 800;
+    private ArrayList<Color> colorUseList = new ArrayList();
+    private ArrayList<Color> colorList = new ArrayList(Arrays.asList(
+            Color.BLUE
+            , Color.CYAN
+            , Color.GREEN
+            , Color.ORANGE
+            , Color.PINK
+            , Color.MAGENTA
+            , Color.YELLOW));
 
-    /* sample
-    public void paint(Graphics g){
-        super.paint(g);  // fixes the immediate problem.
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(tBold, BasicStroke.CAP_ROUND,0));  // 굵기
+    private final int DEFAULT_X = 40;
+    private final int DEFAULT_Y = 40;
+    private final int LINE_WIDTH = 10;
+    private final int LINE_HEIGHT = 10;
 
-        Point p1 = null;
-        Point p2 = null;
-        for(int i=0;i<plist.size();++i){
-            p2 = p1;
-            p1 = plist.get(i);
-
-            if(p2 == null) {
-                g2.setColor(Color.BLUE);
-                g2.fillOval(p1.x - cCircle/2, p1.y - cCircle/2, cCircle,cCircle);
-            }else{
-                g2.setColor(Color.BLUE);
-                g2.fillOval(p2.x - cCircle/2, p2.y - cCircle/2, cCircle,cCircle);
-                Line2D lin = new Line2D.Float(p1, p2);
-
-                if(i+1 == plist.size()){
-                    g2.setColor(Color.BLUE);
-                    g2.fillOval(p1.x - cCircle/2, p1.y - cCircle/2, cCircle,cCircle);
-                }
-                g2.draw(lin);
-            }
-        }
-    }
-    //*/
+    private int tBold = 4;
+    private int cCircle = 8;
+    private int winWidth = 800;
+    private int winHeight = 1400;
 
     //*
     public void paint(Graphics g){
@@ -56,26 +40,97 @@ public class Main extends JFrame{
         Graphics2D g2 = (Graphics2D) g;
         g2.setStroke(new BasicStroke(tBold, BasicStroke.CAP_ROUND,0));  // 굵기
 
-        Point ptop = null;
-        Point pmiddle = null;
-        Point pbottom = null;
+        String upCommitLine = null;
+        String upLine = null;
+        String underLine = null;
 
-        String line1 = null;
-        String line2 = null;
-
+        int cntHeight = 0;
         for(int i=0;i<gitlog.size();++i){
-            String tree = gitlog.get(i).split("commit")[0];
+            String line = gitlog.get(i).split("commit")[0];
             String commit = null;
 
             if(1 < gitlog.get(i).split("commit").length){
                 commit = gitlog.get(i).split("commit")[1];
             }
 
-            line2 = line1;
-            line1 = tree;
+            upLine = underLine;
+            underLine = line;
 
-            if(null == line2){
+            System.out.println(line);
 
+            if(-1 < line.indexOf("*")){
+                /** commit line **/
+                cntHeight = cntHeight+1;
+
+                Line2D lin = null;
+                int cntBranch = line.length() / 2;
+                int commitBranch = line.indexOf("* ") + 2;
+
+                if(0 != commitBranch) commitBranch = commitBranch / 2;
+
+                if(null == upLine){
+                    /** first line **/
+                    int width = DEFAULT_X + (commitBranch * LINE_WIDTH) - (LINE_WIDTH / 2);
+                    int height = DEFAULT_Y + LINE_HEIGHT/2;
+
+                    g2.setColor(colorList.get(0));
+                    g2.fillOval(width - cCircle/2, height - cCircle/2, cCircle ,cCircle);
+                }else{
+                    /** other line **/
+                    int cntUpPoint = upCommitLine.replaceAll(" ","").length();
+                    int cntUpCommitPoint = upCommitLine.replaceAll(" ","").indexOf("*");
+
+                    int cntPoint = underLine.replaceAll(" ","").length();
+                    int cntCommitPoint = underLine.replaceAll(" ","").indexOf("*");
+
+                    upLine = upLine.replaceAll(" ","");
+
+                    /*  "/|" , "|\" , "\|" , "|/" , "|" , " \" , " /"  */
+                    for(int j=0; j<upLine.length() ;++j){
+                        String pipe = upLine.charAt(j)+"";
+
+                        if("|".equals(pipe)){
+                            g2.setColor(colorList.get(0));
+
+                            lin = new Line2D.Float(
+                                    DEFAULT_X + ((j+1) * LINE_WIDTH) - (LINE_WIDTH / 2),
+                                    DEFAULT_Y + (LINE_HEIGHT * (cntHeight-1)) - (LINE_HEIGHT / 2),
+                                    DEFAULT_X + ((j+1) * LINE_WIDTH) - (LINE_WIDTH / 2),
+                                    DEFAULT_Y + (LINE_HEIGHT * cntHeight) - (LINE_HEIGHT / 2));
+
+                            g2.draw(lin);
+
+                        }else if("/".equals(pipe)){
+                            g2.setColor(colorList.get(0));
+
+                            lin = new Line2D.Float(
+                                    DEFAULT_X + ((j+1) * LINE_WIDTH) - (LINE_WIDTH / 2),
+                                    DEFAULT_Y + (LINE_HEIGHT * (cntHeight-1)) - (LINE_HEIGHT / 2),
+                                    DEFAULT_X + (j * LINE_WIDTH) - (LINE_WIDTH / 2),
+                                    DEFAULT_Y + (LINE_HEIGHT * cntHeight) - (LINE_HEIGHT / 2));
+
+                            g2.draw(lin);
+
+                        }else if("\\".equals(pipe)){
+                            g2.setColor(colorList.get(0));
+
+                            lin = new Line2D.Float(
+                                    DEFAULT_X + (j * LINE_WIDTH) - (LINE_WIDTH / 2),
+                                    DEFAULT_Y + (LINE_HEIGHT * (cntHeight-1)) - (LINE_HEIGHT / 2),
+                                    DEFAULT_X + ((j+1) * LINE_WIDTH) - (LINE_WIDTH / 2),
+                                    DEFAULT_Y + (LINE_HEIGHT * cntHeight) - (LINE_HEIGHT / 2));
+
+                            g2.draw(lin);
+                        }
+                    }
+                }
+
+                g2.fillOval(DEFAULT_X + (commitBranch * LINE_WIDTH) - (LINE_WIDTH / 2) - cCircle/2
+                        , DEFAULT_Y + (LINE_HEIGHT * cntHeight) - (LINE_HEIGHT / 2) - cCircle/2
+                        , cCircle
+                        , cCircle);
+
+                upCommitLine = line;
             }
         }
     }
@@ -105,10 +160,6 @@ public class Main extends JFrame{
         }catch(IOException e){
             e.printStackTrace();
         }
-
-        for(String s : gitlog){
-            //System.out.println(s);
-        }
     }
 
     public Main(){
@@ -121,20 +172,6 @@ public class Main extends JFrame{
     }
 
     public static void main(String []args){
-        //*
-        plist.add(new Point(100,100));
-        plist.add(new Point(100,120));
-        plist.add(new Point(120,140));
-        plist.add(new Point(120,160));
-        plist.add(new Point(100,180));
-        plist.add(new Point(100,200));
-        plist.add(new Point(120,220));
-        plist.add(new Point(140,240));
-        plist.add(new Point(140,260));
-        plist.add(new Point(120,280));
-        plist.add(new Point(100,300));
-        //*/
-
         new Main();
     }
 }
